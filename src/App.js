89ec5e81b03370e72e8b918a4c854925dd71d67b
Zahliada VM <waah777@gmail.com>
2024-01-27 21:37:31 +0200

    добавил модуль "proj4"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import proj4 from "proj4";
+
 import logo from "./public/logo.svg";
 import "./App.css";
 
 function App() {
+  console.log("# App/render");
+
+  const proj = new proj4.Proj("EPSG:3857");
+  const xy = proj4.transform(proj4.WGS84, proj, proj4.toPoint([-180, 0]));
+  console.log("xy =", xy);
+
+  const coordIn = [-180, 0];
+  const source = proj4.WGS84;
+  const dest = new proj4.Proj("EPSG:3857");
+  let point = proj4.toPoint(coordIn);
+  const coordOut = proj4.transform(source, dest, point);
+  /*
+  const expect = {
+    x: -20037508.342789244,
+    y: -7.081154551613622e-10,
+  };*/
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +36,10 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <p>cootd in = {JSON.stringify(coordIn)}</p>
+          <p>cootd out = {JSON.stringify(coordOut)}</p>
+        </div>
       </header>
     </div>
   );
